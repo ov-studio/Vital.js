0c31955975d0aebdd9820d0379ec98b29b1fe705
Aviril <89867985+Aviril@users.noreply.github.com>
2022-08-02 18:00:21 +0300

    Update proxy.js

--- src/utilities/proxy.js
+++ src/utilities/proxy.js
@@ -31,19 +31,17 @@ vNetworkify.utility.proxy.addMethod("create", function(data, exec) {
 // Instance Members //
 ///////////////////////
 
-// @Desc: Instance Constructor
-vNetworkify.utility.proxy.addMethod("constructor", function(self, data, exec) {
-    self.buffer = new WeakMap()
-    [self.proxy, self.revoke] = Proxy.revocable(data, {
+function createProxy(self, data) {
+    const cProxy = Proxy.revocable(data, {
         set(data, property, value) {
             data[property] = value
-            exec(data, property, value)
+            self.exec(data, property, value)
             return true
         },
         get(data, property) {
             const value = data[property]
             if (vNetworkify.utility.isObject(value)) {
-                if (!self.buffer.has(value)) self.buffer.set(value, createProxy(value, exec))
+                if (!self.buffer.has(value)) self.buffer.set(value, createProxy(self, value, self.exec))
                 return self.buffer.get(value)
             }
             return value
@@ -54,6 +52,15 @@ vNetworkify.utility.proxy.addMethod("constructor", function(self, data, exec) {
             return true
         }
     })
+    self.revoke.push(cProxy.revoke)
+    return cProxy.proxy
+}
+
+// @Desc: Instance Constructor
+vNetworkify.utility.proxy.addMethod("constructor", function(self, data, exec) {
+    self.buffer = new WeakMap(), self.revoke = []
+    self.data = data, self.exec = exec
+    self.proxy = createProxy(self, self.data)
 }, "isInstance")
 
 // @Desc: Verifies instance's validity
@@ -64,7 +71,26 @@ vNetworkify.utility.proxy.addInstanceMethod("isInstance", function(self) {
 // @Desc: Destroys the instance
 vNetworkify.utility.proxy.addInstanceMethod("destroy", function(self) {
     self.isUnloaded = true
-    self.revoke()
+    for (const i in self.revoke) {
+        const j = self.revoke[i]
+        j()
+    }
     delete self.buffer
     return true
 })
+
+
+// TODO: WIP USAGE
+const test = {
+    testA: "valueA",
+    testB: {
+        testC: "valueC"
+    }
+}
+const wew = vNetworkify.utility.proxy.create(test, function(_, property, value) {
+    console.log(property, value)
+})
+wew.proxy.testNew = "llll"
+wew.proxy.testB.testD = "lol"
+console.log(test)
+console.log(wew.proxy)
