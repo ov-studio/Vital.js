9211f56bd7c8ae592a2b3e1c0859f444f79c56fb
Aviril <89867985+Aviril@users.noreply.github.com>
2022-08-02 17:01:42 +0300

    Refactored proxy to function as 'class'

--- src/utilities/proxy.js
+++ src/utilities/proxy.js
@@ -13,11 +13,28 @@
 // Class: Proxy //
 ///////////////////
 
-// @Desc: Creates a object/class's proxy
-vNetworkify.utility.createProxy = function(data, exec) {
+vNetworkify.utility.proxy = CUtility.createClass({})
+
+
+/////////////////////
+// Static Members //
+/////////////////////
+
+// @Desc: Creates a fresh proxy on desired object/class
+vNetworkify.utility.proxy.addMethod("create", function(data, exec) {
     if ((!vNetworkify.utility.isObject(data) && !vNetworkify.utility.isClass(data)) || !vNetworkify.utility.isFunction(exec)) return false
-    const cBuffer = new WeakMap()
-    return new Proxy(data, {
+    return new vNetworkify.utility.proxy(data, exec)
+})
+
+
+///////////////////////
+// Instance Members //
+//////////////////////
+
+// @Desc: Instance Constructor
+vNetworkify.utility.proxy.addMethod("constructor", function(self, data, exec) {
+    self.buffer = new WeakMap()
+    {self.proxy, self.revoke} = Proxy.revocable(data, {
         set(data, property, value) {
             data[property] = value
             exec(data, property, value)
@@ -26,15 +43,28 @@ vNetworkify.utility.createProxy = function(data, exec) {
         get(data, property) {
             const value = data[property]
             if (vNetworkify.utility.isObject(value)) {
-                if (!cBuffer.has(value)) cBuffer.set(value, createProxy(value, exec))
-                return cBuffer.get(value)
+                if (!self.buffer.has(value)) self.buffer.set(value, createProxy(value, exec))
+                return self.buffer.get(value)
             }
             return value
         },
         deleteProperty(data, property) {
             const value = data[property]
-            if (vNetworkify.utility.isObject(value) && cBuffer.has(value)) cBuffer.delete(value)
+            if (vNetworkify.utility.isObject(value) && self.buffer.has(value)) self.buffer.delete(value)
             return true
         }
     })
-}
+}, "isInstance")
+
+// @Desc: Verifies instance's validity
+vNetworkify.utility.proxy.addInstanceMethod("isInstance", function(self) {
+    return (!self.isUnloaded && true) || false
+})
+
+// @Desc: Destroys the instance
+vNetworkify.utility.proxy.addInstanceMethod("destroy", function(self) {
+    self.isUnloaded = true
+    self.revoke()
+    delete self.buffer
+    return true
+})
