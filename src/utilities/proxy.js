48ba911dcae0c5a6551b6c17831da31993c609e0
Aviril <89867985+Aviril@users.noreply.github.com>
2022-09-02 19:55:45 +0300

    Upgrading proxy to work w/ latest vnetworkify

--- src/utilities/proxy.js
+++ src/utilities/proxy.js
@@ -13,7 +13,8 @@
 // Class: Proxy //
 ///////////////////
 
-vNetworkify.utility.proxy = vNetworkify.utility.createClass({})
+const CProxy = vNetworkify.utility.createClass({})
+vNetworkify.utility.proxy = CProxy.public
 
 
 /////////////////////
@@ -21,9 +22,9 @@ vNetworkify.utility.proxy = vNetworkify.utility.createClass({})
 /////////////////////
 
 // @Desc: Creates a fresh proxy on desired object/class
-vNetworkify.utility.proxy.addMethod("create", function(data, exec) {
+CProxy.public.addMethod("create", function(data, exec) {
     if ((!vNetworkify.utility.isObject(data) && !vNetworkify.utility.isClass(data)) || !vNetworkify.utility.isFunction(exec)) return false
-    return new vNetworkify.utility.proxy(data, exec)
+    return new CProxy(data, exec)
 })
 
 
@@ -58,19 +59,19 @@ const onProxyInitialize = function(self, data) {
 }
 
 // @Desc: Instance constructor
-vNetworkify.utility.proxy.addMethod("constructor", function(self, data, exec) {
+CProxy.public.addMethod("constructor", function(self, data, exec) {
     self.buffer = new WeakMap(), self.revoke = []
     self.data = data, self.exec = exec
     self.proxy = onProxyInitialize(self, self.data)
 }, "isInstance")
 
 // @Desc: Verifies instance's validity
-vNetworkify.utility.proxy.addInstanceMethod("isInstance", function(self) {
+CProxy.public.addInstanceMethod("isInstance", function(self) {
     return (!self.isUnloaded && true) || false
 })
 
 // @Desc: Destroys the instance
-vNetworkify.utility.proxy.addInstanceMethod("destroy", function(self) {
+CProxy.public.addInstanceMethod("destroy", function(self) {
     self.isUnloaded = true
     for (const i in self.revoke) {
         self.revoke[i]()
